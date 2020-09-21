80ba0d8ad4a7ea79585e0abb3dc59d0a7626153d
azharshahstl <azharshahstl@yahoo.com>
2020-09-21 14:17:36 -0500

    Changed cors.rb URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://git.heroku.com/knows-pickers-app-api.git'
 
     resource '*',
       headers: :any,
